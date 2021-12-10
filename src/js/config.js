be11fe5269943555d2caebfc48226db17cb7486b
chand1625 <chandnagriya25@gmail.com>
2021-12-10 13:35:23 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '747e97d2-7e5a-4b58-a9ef-370c3f1ed4ad';
+export const MODAL_CLOSE_SEC = 2.5;
